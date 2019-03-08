a5e45c00e5c3887a04b38883f356729e81ae5923
Ivan Volodin <ivanwolodin@gmail.com>
2019-03-08 18:23:41 +0500

    Added some primitive validation

--- widget.cpp
+++ widget.cpp
@@ -24,10 +24,45 @@ Widget::~Widget()
 
 void Widget::outputProcessedText()
 {
-    //int raw_int = ui->raw_text->toPlainText().toInt();
     QString raw_string = ui->raw_text->toPlainText();
-    // implement exception processing
+    raw_string = raw_string.remove(QRegExp("[«»^]"));
+    int case_to_act = dataValidation(raw_string);
+    qDebug()<<case_to_act;
+    switch ( case_to_act ) {
+     case 1:
+       ui->processed_text->setText(converter_obj->toBinary(raw_string.toInt()));
+       break;
+     case 2:
+       ui->processed_text->setText(converter_obj->sortingByPetrolStations(raw_string));
+       break;
+     case 3:
+       ui->processed_text->setText(converter_obj->sortingDebtorsByFloor(raw_string));
+       break;
+    case -1:
+      ui->processed_text->setText("Invalid input data");
+      break;
+     }
+}
 
-    //ui->processed_text->setText(converter_obj->toBinary(raw_int));
-     ui->processed_text->setText(converter_obj->sortingByPetrolStations(raw_string));
+int dataValidation(QString raw_data)
+{   /*
+     * primitive data validation
+     * the most primitive validation
+     *
+     * */
+    QStringList myStringList = raw_data.split('\n'); // got all string devided by '\n'
+    myStringList.removeAll(QString(""));
+    if (myStringList.length() == 1){
+        return 1;
+    }
+    QStringList parsed = myStringList[1].split(QRegExp("\\s+"), QString::SkipEmptyParts);
+    if (parsed.length() == 5){
+        return 2;
+    }
+    else if (parsed.length() == 3) {
+        return 3;
+    }
+    else {
+        return -1;
+    }
 }
